c7f56d240e57fc42a9f8e958e268dd08d0f74a68
rspitler <rspitler@gmail.com>
2021-05-21 15:56:35 -0600

    suppress aws output and region warning

--- validate_template.sh
+++ validate_template.sh
@@ -1,6 +1,7 @@
 set -e
+export REGION=us-east-1
 for dir in */; do
   echo $dir
   infra-buddy  validate-template --service-template-directory $dir
-  aws cloudformation validate-template --template-body file://$dir/cloudformation.template
+  aws cloudformation validate-template --template-body file://$dir/cloudformation.template > out
 done
