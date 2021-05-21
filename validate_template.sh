81d8c644d41fc6abcf92dae1edb9011f4bde978a
rspitler <rspitler@gmail.com>
2021-05-21 16:22:03 -0600

    simplify cluster tempalte to remove non-fargate use.  Clean up with linter and integrate into build

--- validate_template.sh
+++ validate_template.sh
@@ -4,4 +4,5 @@ for dir in */; do
   echo $dir
   infra-buddy  validate-template --service-template-directory $dir
   aws cloudformation validate-template --template-body file://$dir/cloudformation.template > out
+  cfn-lint  $dir/cloudformation.template
 done
