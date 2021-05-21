b21033e29ff868663900294ffba31c6f95fbcb92
rspitler <rspitler@gmail.com>
2021-05-21 15:13:54 -0600

    Add github action and aws validate action as well.

--- validate_template.sh
+++ validate_template.sh
@@ -4,4 +4,5 @@ set -e
 for dir in */; do
   echo $dir
   infra-buddy  validate-template --service-template-directory $dir
+  aws cloudformation validate-template --template-body file://$dir/cloudformation.template
 done
