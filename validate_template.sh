4f36e13378232db5ada25f526332757af9dd336d
rspitler <rspitler@gmail.com>
2021-05-28 14:32:20 -0600

    dont run a priviledged action in teh build

--- validate_template.sh
+++ validate_template.sh
@@ -4,6 +4,6 @@ export AWS_DEFAULT_REGION=us-east-1
 for dir in */; do
   echo $dir
   infra-buddy  validate-template --service-template-directory $dir
-  aws cloudformation validate-template --template-body file://$dir/cloudformation.template > out
+#  aws cloudformation validate-template --template-body file://$dir/cloudformation.template > out
   cfn-lint  $dir/cloudformation.template
 done
